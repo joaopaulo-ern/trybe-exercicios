58664cffe7871aa939900e5aa8058f71ade51eeb
14141414141414 <joaopaulo.ern@gmail.com>
2021-11-29 16:26:34 -0300

    exercicios 4.2 sem pratica

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/for of names.js
@@ -0,0 +1,5 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let array of names) {
+    console.log(array)
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/menu.js
@@ -0,0 +1,18 @@
+/*1*/
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
+
+/*2*/
+menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+/*3*/
+menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu[4] = 'Contatos';
+
+console.log(menu);
