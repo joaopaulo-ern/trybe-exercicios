746a24f5f62ee5bd542ba4c8fab4f8d7c39d04fa
jp <joaopaulo.ern@gmail.com>
2022-01-23 13:40:22 -0300

    fim exercicios/8.3

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/teste.js
@@ -0,0 +1,16 @@
+// const objPeople = [
+//   { name: 'José', age: 21 },
+//   { name: 'Lucas', age: 19 },
+//   { name: 'Maria', age: 16 },
+//   { name: 'Gilberto', age: 18 },
+//   { name: 'Vitor', age: 15 },
+// ];
+
+// const verifyAgeDrive = (arrayOfPeople) => {
+//   let filtro = arrayOfPeople.filter((people) => (people.age < 18))
+//   let names = []
+//   filtro.forEach((element, i) => names[i] = (element.name));
+//   return names;
+// };
+// console.log(verifyAgeDrive(objPeople));
+/////////////////////////////////////////////////////////////////////////////
