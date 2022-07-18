d10a4178bbc375de249ad9325fe89fd2b7346f8e
14141414141414 <joaopaulo.ern@gmail.com>
2022-07-18 14:27:24 -0300

    fixação sequelize

---
+++ app-with-sequelize/models/user.js
@@ -0,0 +1,13 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+
+  return User;
+};
+
+module.exports = User;
